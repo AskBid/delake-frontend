2ac58d9d7a13f96c258b66dbfd002980e09990b6
Sergio Marelli <sergio.mrll@gmail.com>
2021-01-04 23:22:27 +0000

    add first normalization to angular values of a set of values

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
 var arc = d3.arc();
+const pool_sizes = make_angular([20,60,5,1,34,36,1,0.5,5,0.3,40,33,12,4,2,3.4,43])
 
 var chart = d3.select("body").append("svg:svg")
-    .attr("class", "chart")
-    .attr("width", 420)
-    .attr("height", 420).append("svg:g")
-    .attr("transform", "translate(200,200)");
+  .attr("class", "chart")
+  .attr("width", 420)
+  .attr("height", 420).append("svg:g")
+  .attr("transform", "translate(200,200)");
 
 chart.selectAll("path")
-    .data([250,50,60])
-    .enter().append("svg:path")
-    .attr("fill", `rgba(0, 130, 0, 0.5)`)
-    .attr("d", function(d){
-        return arc({innerRadius: 50,
-          outerRadius: d,
-          startAngle: 0,
-          endAngle: Math.PI + (Math.PI / 2)})
-    });
+  .data(pool_sizes)
+  .enter().append("svg:path")
+  .attr("fill", `rgba(0, 130, 0, 0.5)`)
+  .attr("d", function(d, i){
+    return arc({innerRadius: 70,
+      outerRadius: 85+i,
+      startAngle: pool_sizes[i - 1],
+      endAngle: d})
+  });
+
+
+function make_angular(values) {
+  const sum = values.reduce((a, b) => a + b, 0);
+  const t = Math.PI * 2;
+  const angular = values.map((pool_size) => {
+    return (pool_size / sum) * t
+  });
+  return angular
+}
