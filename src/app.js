403b0bf4ab44af6b4facb3d09868048459352cfb
Sergio Marelli <sergio.mrll@gmail.com>
2021-01-05 19:41:45 +0000

    circle is now aware of minimum dimension

--- src/app.js
+++ src/app.js
@@ -1,24 +1,29 @@
 const arc = d3.arc();
-const pool_sizes = make_angular([20,60,5,1,34,36,1,0.5,5,0.3,40,33,12,4,2,3.4,43])
+let pool_sizes = make_angular([20,60,5,1,34,36,1,0.5,5,0.3,40,33,12,4,2,3.4,43])
 
-const svg = d3.select(".container").append("svg");
+let svg = d3.select(".container").append("svg");
 
 let width = document.getElementsByClassName("container")[0].offsetWidth
-let height = document.getElementsByClassName("container")[0].offsetHeight   
+let height = document.getElementsByClassName("container")[0].offsetHeight 
+let minimum_dimension = Math.min(width, height)
 
 svg.attr("width", '100%')
   .attr("height",  '100%')
   // .attr("transform", "translate(200,200)")
-  .style("background", "#ccc")
+  .style("background", "#eee  ")
 
-svg.selectAll("path")
+let g = svg.append('g')
+  .attr("transform", `translate(${width / 2}, ${height / 2})`)
+
+g.selectAll("path")
   .data(pool_sizes)
   .enter()
   .append("path")
   .attr("fill", (d) => {return `rgba(${d.start * 30}, ${d.end * 30}, ${d.end * 30}, 0.5)`})
   .attr("d", function(d, i){
-    return arc({innerRadius: 119,
-      outerRadius: 125,
+    return arc({
+      outerRadius: (minimum_dimension/2)-50,
+      innerRadius: (minimum_dimension/2)-60,
       startAngle: d.start,
       endAngle: d.end,
       padAngle: 0.006,
